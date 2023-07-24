306db3c64601832771ae82d3e0fb48096e9924ab
Michele Cafagna <m.cafagna26@gmail.com>
2023-07-24 11:04:12 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+import os
+
+import pkg_resources
+from setuptools import setup, find_packages
+
+setup(
+    name="clipcap",
+    py_modules=["clipcap"],
+    version="1.0",
+    description="",
+    author="Michele Cafagna",
+    packages=find_packages(["notebooks*"]),
+    install_requires=[
+        str(r)
+        for r in pkg_resources.parse_requirements(
+            open(os.path.join(os.path.dirname(__file__), "requirements.txt"))
+        )
+    ],
+    include_package_data=True,
+)
