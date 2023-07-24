09d985a5980c509c3cd4c5dae0bddb7af626a091
Michele Cafagna <m.cafagna26@gmail.com>
2023-07-24 12:09:11 +0200

    added generate

--- clipcap/clipcap.py
+++ clipcap/clipcap.py
@@ -83,6 +83,77 @@ class ClipCaptionModel(nn.Module):
                 )
             )
 
+    def generate(
+        self,
+        tokenizer=None,
+        tokens=None,
+        prompt=None,
+        embed=None,
+        entry_count=1,
+        entry_length=67,  # maximum number of words
+        top_p=0.8,
+        temperature=1.0,
+        stop_token: str = ".",
+    ):
+
+        if tokenizer:
+            self.tokenizer = tokenizer
+        else:
+            if not self.tokenizer:
+                raise ValueError(f"You need to pass a valid tokenizer. You passed {self.tokenizer}")
+
+        self.eval()
+        generated_num = 0
+        generated_list = []
+        stop_token_index = tokenizer.encode(stop_token)[0]
+        filter_value = -float("Inf")
+        device = next(self.parameters()).device
+
+        with torch.no_grad():
+
+            for entry_idx in range(entry_count):
+                if embed is not None:
+                    generated = embed
+                else:
+                    if tokens is None:
+                        tokens = torch.tensor(tokenizer.encode(prompt))
+                        tokens = tokens.unsqueeze(0).to(device)
+
+                    generated = self.gpt.transformer.wte(tokens)
+
+                for i in range(entry_length):
+
+                    outputs = self.gpt(inputs_embeds=generated)
+                    logits = outputs.logits
+                    logits = logits[:, -1, :] / (temperature if temperature > 0 else 1.0)
+                    sorted_logits, sorted_indices = torch.sort(logits, descending=True)
+                    cumulative_probs = torch.cumsum(
+                        nnf.softmax(sorted_logits, dim=-1), dim=-1
+                    )
+                    sorted_indices_to_remove = cumulative_probs > top_p
+                    sorted_indices_to_remove[..., 1:] = sorted_indices_to_remove[
+                        ..., :-1
+                    ].clone()
+                    sorted_indices_to_remove[..., 0] = 0
+
+                    indices_to_remove = sorted_indices[sorted_indices_to_remove]
+                    logits[:, indices_to_remove] = filter_value
+                    next_token = torch.argmax(logits, -1).unsqueeze(0)
+                    next_token_embed = self.gpt.transformer.wte(next_token)
+                    if tokens is None:
+                        tokens = next_token
+                    else:
+                        tokens = torch.cat((tokens, next_token), dim=1)
+                    generated = torch.cat((generated, next_token_embed), dim=1)
+                    if stop_token_index == next_token.item():
+                        break
+
+                output_list = list(tokens.squeeze().cpu().numpy())
+                output_text = tokenizer.decode(output_list)
+                generated_list.append(output_text)
+
+        return generated_list[0]
+
     def generate_beam(self, tokenizer=None,
                      beam_size: int = 5,
                      prompt=None,
